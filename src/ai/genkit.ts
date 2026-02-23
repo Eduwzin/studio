57704866140d8cad2f048314644a957d8af663bc
PedroEvaristto <pevaristooo@gmail.com>
2026-02-23 21:07:10 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch from https

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,4 +5,4 @@ export const ai = genkit({
   plugins: [googleAI()],
 });
 
-export const geminiModel = googleAI.model('gemini-1.5-pro-latest');
+export const geminiModel = googleAI.model('gemini-pro');
