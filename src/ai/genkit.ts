1dce79b5971d8e8780a93e9233aff507f26b7f84
PedroEvaristto <pevaristooo@gmail.com>
2026-02-23 20:39:44 +0000

    Try fixing this error: `Build Error: Export gemini25Flash doesn't exist

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,8 @@
 import {genkit} from 'genkit';
-import {vertexAI, gemini25Flash} from '@genkit-ai/vertexai';
+import {vertexAI, gemini20Flash} from '@genkit-ai/vertexai';
 
 export const ai = genkit({
   plugins: [vertexAI({location: 'us-central1'})],
 });
 
-export const geminiModel = gemini25Flash;
+export const geminiModel = gemini20Flash;
