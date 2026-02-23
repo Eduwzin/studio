f3d216fa746d1faec2959998b087dc01b1f48895
PedroEvaristto <pevaristooo@gmail.com>
2026-02-23 20:51:54 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type {NextConfig} from 'next';
-const { withGenkit } = require('@genkit-ai/next');
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -33,4 +32,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withGenkit(nextConfig);
+export default nextConfig;
